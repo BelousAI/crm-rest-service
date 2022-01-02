5aeac8bce0835f11a28b03c3960bf2a3690db4a3
BelousAnton <anton.belous@customertimes.com>
2022-01-02 17:09:09 +0300

    Implement Add Customer

--- src/main/java/com/belous/springdemo/controller/CustomerRestController.java
+++ src/main/java/com/belous/springdemo/controller/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.belous.springdemo.controller;
 import com.belous.springdemo.entity.Customer;
 import com.belous.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,17 +15,30 @@ public class CustomerRestController {
     @Autowired
     CustomerService customerService;
 
-    // add mapping for GET /customers
+    // add mapping for GET /customers - get list of customers
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
 
         return customerService.getCustomers();
     }
 
-    // add mapping for GET /customers/{customerId}
+    // add mapping for GET /customers/{customerId} - get single customer
     @GetMapping("/customers/{customerId}")
     public Customer getCustomer(@PathVariable int customerId) {
 
         return customerService.getCustomer(customerId);
+    }
+
+    // add mapping for POST /customers - add new customer
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer theCustomer) {
+
+        // also just in case the pass an id in json ... set id to 0
+        // this is force a save of new item ... instead of update
+
+        theCustomer.setId(0);
+        customerService.saveCustomer(theCustomer);
+
+        return theCustomer;
     }
 }
